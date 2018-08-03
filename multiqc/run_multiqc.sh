73b7116acac1bbeaf6ab3cdd742a9531fd848422
Xin Li <usigwt@gmail.com>
2018-08-02 23:57:35 -0700

    Thu Aug  2 23:57:34 PDT 2018

---
+++ multiqc/run_multiqc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# module load multiqc/1.5
+# multiqc ../fastqc/output ../rna_seqc/rna_seqc_output ../star/output_bam ../rsem/rsem_output ../picard/picard_output
+multiqc ../data_scratch/fastqc ../data_scratch/rna_seqc ../data_scratch/star ../data_scratch/rsem ../data_scratch/picard
+
+
