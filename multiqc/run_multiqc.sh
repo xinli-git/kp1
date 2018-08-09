d6d18a5eaa952e3773cf54b45f099ef103ffab87
Xin Li <usigwt@gmail.com>
2018-08-09 10:08:08 -0700

    Thu Aug  9 10:08:06 PDT 2018

--- multiqc/run_multiqc.sh
+++ multiqc/run_multiqc.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 
-module load multiqc/1.5
+# module load multiqc/1.5
 # multiqc ../fastqc/output ../rna_seqc/rna_seqc_output ../star/output_bam ../rsem/rsem_output ../picard/picard_output
+
+
+# export PATH="/users/nsabell/software/MultiQC/:$PATH"
+# /users/nsabell/software/MultiQC/scripts/multiqc 
 multiqc ../data_scratch/fastqc ../data_scratch/rna_seqc ../data_scratch/star ../data_scratch/rsem ../data_scratch/picard
 
 
