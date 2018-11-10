f780f7cf078866b13fff2f08511ed9234668dce3
Xin Li <usigwt@gmail.com>
2018-11-09 19:44:45 -0800

    Fri Nov  9 19:44:45 PST 2018

--- rsem/rsem_kp1.sh
+++ rsem/rsem_kp1.sh
@@ -3,10 +3,12 @@
 #RSEM quantification on filtered bam files (transcriptome ref) for rare disease samples
 input_bam=$1 #.Aligned.toTranscriptome.out_mapq30_sorted_dedup.bam format
 rsem_dir=~/tools/RSEM-1.3.0/
-rsem_reference_prefix=~/projects/quantification/rsem/rsem_reference/Mus_musculus.GRCm38.92.vM17
+rsem_reference_prefix=~/projects/quantification/rsem/rsem_reference/Mus_musculus.GRCm38.92.vM17/Mus_musculus.GRCm38.92.vM17
 N_THREADS=4
 sample_name=$2
 log_file=${sample_name}_rsem.log
+
+
 
 
 date>$log_file
@@ -17,7 +19,7 @@ cmd="$rsem_dir/rsem-calculate-expression \
 	--paired-end \
 	--no-bam-output \
 	--estimate-rspd \
-	--strand-specific \
+	--strandedness reverse \
 	--seed 12345 \
 	--alignments \
 	$input_bam \
@@ -29,5 +31,10 @@ echo $cmd >>$log_file
 eval $cmd >>$log_file 2>&1
 
 date>> $log_file
+
+
+# --strandedness <none|forward|reverse>
+# This option defines the strandedness of the RNA-Seq reads. It recognizes three values: 'none', 'forward', and 'reverse'. 'none' refers to non-strand-specific protocols. 'forward' means all (upstream) reads are derived from the forward strand. 'reverse' means all (upstream) reads are derived from the reverse strand. If 'forward'/'reverse' is set, the '--norc'/'--nofw' Bowtie/Bowtie 2 option will also be enabled to avoid aligning reads to the opposite strand. For Illumina TruSeq Stranded protocols, please use 'reverse'. (Default: 'none')
+
 
 
